9b2ccb731a081c4b40f5c7803a8f7f79e1e01eb4
sdalvai <simon.dalvai@gmail.com>
2020-01-21 10:50:15 +0100

    renamed MonochromeImageCreator.java to ImageUtil.java, because it handles ALL actions needed with Images

--- src/main/java/it/noi/edisplay/utils/ImageUtil.java
+++ src/main/java/it/noi/edisplay/utils/ImageUtil.java
@@ -3,6 +3,7 @@ package it.noi.edisplay.utils;
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
@@ -18,4 +19,16 @@ public class MonochromeImageCreator {
         baos.flush();
         return baos.toByteArray();
     }
+
+    public static String getBinaryImage(BufferedImage image) {
+        StringBuffer result = new StringBuffer();
+        int width = image.getWidth();
+        int height = image.getHeight();
+        for (int i = 0; i < height; i++)
+            for (int j = 0; j < width; j++)
+                result.append(image.getRGB(j, i) == -1?0:1 );
+        return result.toString();
+    }
+
+
 }
