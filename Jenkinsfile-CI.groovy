a284be69d2b70ad07a8e3b3778d7d71db8818be1
Peter Moser <p.moser@noi.bz.it>
2020-01-30 10:59:01 +0100

    CI: Create a docker pipeline

--- Jenkinsfile-CI.groovy
+++ Jenkinsfile-CI.groovy
@@ -1,15 +1,30 @@
 pipeline {
-    agent {
-        dockerfile {
-            filename 'docker/dockerfile-java'
-            additionalBuildArgs '--build-arg JENKINS_USER_ID=`id -u jenkins` --build-arg JENKINS_GROUP_ID=`id -g jenkins`'
+    agent any
+
+	stages {
+        stage('Configure') {
+            steps {
+                sh 'cp .env.example .env'
+            }
+        }
+    }
+
+	stages {
+        stage('Build') {
+            steps {
+				sh '''
+					docker-compose build --pull --build-arg JENKINS_USER_ID=`id -u jenkins` --build-arg JENKINS_GROUP_ID=`id -g jenkins`
+				'''
+            }
         }
     }
 
     stages {
         stage('Test') {
             steps {
-                sh 'mvn -B -U clean test verify'
+				sh '''
+					docker-compose run --rm -u `id -u jenkins`:`id -g jenkins` java mvn -B -U clean test
+				'''
             }
         }
     }
