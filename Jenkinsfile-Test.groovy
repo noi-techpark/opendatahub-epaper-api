23f4a5b7494f110a7e30f9a1cecd50d4d9d52856
Peter Moser <p.moser@noi.bz.it>
2020-01-30 13:44:26 +0100

    CD: Jenkinsfile for Testing

--- Jenkinsfile-Test.groovy
+++ Jenkinsfile-Test.groovy
@@ -4,21 +4,30 @@ pipeline {
     environment {
         AWS_ACCESS_KEY_ID = credentials('AWS_ACCESS_KEY_ID')
         AWS_SECRET_ACCESS_KEY = credentials('AWS_SECRET_ACCESS_KEY')
-		DOCKER_IMAGE = '755952719952.dkr.ecr.eu-west-1.amazonaws.com/e-paper-displays-api'
+		DOCKER_PROJECT_NAME = "e-ink-displays-api"
+		DOCKER_SERVER_IP = "63.33.73.203"
+        DOCKER_SERVER_DIRECTORY = "/var/docker/e-ink-displays-api"
+		DOCKER_IMAGE = '755952719952.dkr.ecr.eu-west-1.amazonaws.com/e-ink-displays-api'
 		DOCKER_TAG = "test-$BUILD_NUMBER"
 
         DB_URL = "jdbc:postgresql://test-pg-bdp.co90ybcr8iim.eu-west-1.rds.amazonaws.com:5432/epaper"
         DB_USERNAME = "epaper"
         DB_PASSWORD = credentials('epaper-api-test-db-password')
-        SERVER_ADDR = "0.0.0.0"
-        SERVER_PORT = 8080
-        APP_DEBUG = false
     }
 
 	stages {
         stage('Configure') {
             steps {
-                sh 'cp .env.example .env'
+                sh '''
+					rm -f .env
+					cp .env.example .env
+                	echo 'COMPOSE_PROJECT_NAME=${DOCKER_PROJECT_NAME}' >> .env
+                	echo 'DOCKER_IMAGE_APP=${DOCKER_IMAGE_APP}' >> .env
+                	echo 'DOCKER_TAG_APP=${DOCKER_TAG_APP}' >> .env
+					echo 'DB_URL=${DB_URL}' >> .env
+					echo 'DB_USERNAME=${DB_USERNAME}' >> .env
+					echo 'DB_PASSWORD=${DB_PASSWORD}' >> .env
+				'''
             }
         }
 
@@ -39,12 +48,27 @@ pipeline {
 				'''
             }
         }
-		// stage('Deploy') {
-        //     steps {
-		// 		sh '''
+		stage('Deploy') {
+            steps {
+               sshagent(['jenkins-ssh-key']) {
+                    sh """
+					    ssh -o StrictHostKeyChecking=no ${DOCKER_SERVER_IP} 'AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}" AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}" bash -c "aws ecr get-login --region eu-west-1 --no-include-email | bash"'
 
-		// 		'''
-        //     }
-        // }
+						ssh -o StrictHostKeyChecking=no ${DOCKER_SERVER_IP} 'mkdir -p ${DOCKER_SERVER_DIRECTORY}'
+
+                    	ssh -o StrictHostKeyChecking=no ${DOCKER_SERVER_IP} 'ls -1t ${DOCKER_SERVER_DIRECTORY}/releases/ | tail -n +10 | grep -v `readlink -f ${DOCKER_SERVER_DIRECTORY}/current | xargs basename --` -- | xargs -r printf \"${DOCKER_SERVER_DIRECTORY}/releases/%s\\n\" | xargs -r rm -rf --'
+
+                    	ssh -o StrictHostKeyChecking=no ${DOCKER_SERVER_IP} 'mkdir -p ${DOCKER_SERVER_DIRECTORY}/releases/${BUILD_NUMBER}'
+                    	pv docker-compose.run.yml | ssh -o StrictHostKeyChecking=no ${DOCKER_SERVER_IP} 'tee ${DOCKER_SERVER_DIRECTORY}/releases/${BUILD_NUMBER}/docker-compose.run.yml'
+                    	pv .env | ssh -o StrictHostKeyChecking=no ${DOCKER_SERVER_IP} 'tee ${DOCKER_SERVER_DIRECTORY}/releases/${BUILD_NUMBER}/.env'
+                    	ssh -o StrictHostKeyChecking=no ${DOCKER_SERVER_IP} 'cd ${DOCKER_SERVER_DIRECTORY}/releases/${BUILD_NUMBER} && docker-compose pull'
+
+                    	ssh -o StrictHostKeyChecking=no ${DOCKER_SERVER_IP} '[ -d \"${DOCKER_SERVER_DIRECTORY}/current\" ] && (cd ${DOCKER_SERVER_DIRECTORY}/current && docker-compose down) || true'
+                    	ssh -o StrictHostKeyChecking=no ${DOCKER_SERVER_IP} 'ln -sfn ${DOCKER_SERVER_DIRECTORY}/releases/${BUILD_NUMBER} ${DOCKER_SERVER_DIRECTORY}/current'
+                    	ssh -o StrictHostKeyChecking=no ${DOCKER_SERVER_IP} 'cd ${DOCKER_SERVER_DIRECTORY}/current && docker-compose up --detach'
+					"""
+                }
+            }
+        }
     }
 }
