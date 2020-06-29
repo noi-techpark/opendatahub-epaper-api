bda44c66a86af10fc57b123b1f870b4f2a506fd8
Alex Lanz <alex@krumer.it>
2020-06-29 15:42:46 +0200

    Fixing deployment

--- infrastructure/Jenkinsfile-Test.groovy
+++ infrastructure/Jenkinsfile-Test.groovy
@@ -35,7 +35,7 @@ pipeline {
             steps {
                 sh '''
                     docker-compose --no-ansi build --pull --build-arg JENKINS_USER_ID=$(id -u jenkins) --build-arg JENKINS_GROUP_ID=$(id -g jenkins)
-                    docker-compose --no-ansi run --rm --no-deps -u $(id -u jenkins):$(id -g jenkins) "app mvn -U -B clean test"
+                    docker-compose --no-ansi run --rm --no-deps -u $(id -u jenkins):$(id -g jenkins) app "mvn -U -B clean test"
                 '''
             }
         }
