c3509d1d5376f0521f3727ea59949a65434ae257
dulvui <simon.dalvai@gmail.com.com>
2020-09-18 11:18:37 +0200

    Image util checks now length of description and splits it in two lines if it deosn't fit the screen

--- src/main/java/it/noi/edisplay/utils/ImageUtil.java
+++ src/main/java/it/noi/edisplay/utils/ImageUtil.java
@@ -3,6 +3,7 @@ package it.noi.edisplay.utils;
 import it.noi.edisplay.dto.EventDto;
 import it.noi.edisplay.model.Resolution;
 import org.imgscalr.Scalr;
+
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
@@ -41,9 +42,9 @@ public class ImageUtil {
 		for (int i = 0; i < height; i++)
 			for (int j = 0; j < width; j++)
 				if (inverted)
-					result.append(bufferedImage.getRGB(j, i) == -1 ? 1 : 0);
-				else
 					result.append(bufferedImage.getRGB(j, i) == -1 ? 0 : 1);
+				else
+					result.append(bufferedImage.getRGB(j, i) == -1 ? 1 : 0);
 
 		return result.toString();
 	}
@@ -59,18 +60,58 @@ public class ImageUtil {
 
 		Graphics graphics = bufferedImage.getGraphics();
 		graphics.setColor(Color.BLACK);
-		graphics.setFont(new Font("Monospaced", Font.PLAIN, 30));
-		graphics.drawString(eventDto.getSpaceDesc(), 20, 50);
-		graphics.drawString(eventDto.getEventDescriptionEN(), 20, 225);
-		graphics.setFont(new Font("Monospaced", Font.PLAIN, 15));
-		graphics.drawString(eventDto.getCompanyName(), 20, 90);
-		graphics.setFont(new Font("Monospaced", Font.PLAIN, 20));
-		DateFormat formatter = new SimpleDateFormat("HH:mm");
-		formatter.setTimeZone(TimeZone.getTimeZone("UTC"));
-		graphics.drawString(formatter.format(eventDto.getEventStartDateUTC()), 400, 330);
-		graphics.drawString("to", 470, 330);
-		graphics.drawString(formatter.format(eventDto.getEventEndDateUTC()), 500, 330);
+		graphics.setFont(new Font("Monospaced", Font.PLAIN, 26));
 
+		graphics.drawString(eventDto.getSpaceDesc(), 20, 50);
+
+		//check if description fits, else split in two lines
+		if (eventDto.getEventDescriptionEN().length() < 35)
+			graphics.drawString(eventDto.getEventDescriptionEN(), 20, 225);
+		else {
+			StringBuilder firstLine = new StringBuilder();
+			StringBuilder secondLine = new StringBuilder();
+			String[] descriptions = eventDto.getEventDescriptionEN().split(" ");
+			for (int i = 0; i < descriptions.length; i++) {
+				if (i < descriptions.length / 2)
+					firstLine.append(descriptions[i]).append(" ");
+				else
+					secondLine.append(descriptions[i]).append(" ");
+			}
+			graphics.drawString(firstLine.toString(), 20, 200);
+			graphics.drawString(secondLine.toString(), 20, 250);
+		}
+		graphics.setFont(new Font("Monospaced", Font.PLAIN, 18));
+
+
+		//check if  company name fits, else split in two lines
+		if (eventDto.getCompanyName().length() < 35)
+			graphics.drawString(eventDto.getCompanyName(), 20, 90);
+		else {
+			StringBuilder firstLine = new StringBuilder();
+			StringBuilder secondLine = new StringBuilder();
+			String[] descriptions = eventDto.getCompanyName().split(" ");
+			for (int i = 0; i < descriptions.length; i++) {
+				if (i < descriptions.length / 2)
+					firstLine.append(descriptions[i]).append(" ");
+				else
+					secondLine.append(descriptions[i]).append(" ");
+			}
+			graphics.drawString(firstLine.toString(), 20, 90);
+			graphics.drawString(secondLine.toString(), 20, 130);
+		}
+
+
+		graphics.setFont(new Font("Monospaced", Font.PLAIN, 20));
+
+		DateFormat formatter = new SimpleDateFormat("HH:mm");
+		formatter.setTimeZone(TimeZone.getTimeZone("GMT"));
+		graphics.drawString(formatter.format(eventDto.getEventStartDateUTC() + 7200000), 400, 330); //TODO replace 7200000 with correct time conversion
+		graphics.drawString("to", 470, 330);
+		graphics.drawString(formatter.format(eventDto.getEventEndDateUTC() + 7200000), 500, 330);
+
+		DateFormat dayFormatter = new SimpleDateFormat("dd/MM");
+		formatter.setTimeZone(TimeZone.getTimeZone("GMT"));
+		graphics.drawString(dayFormatter.format(eventDto.getEventStartDateUTC() + 7200000), 320, 330);
 
 		return convertToMonochrome(bufferedImage);
 	}
