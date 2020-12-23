9ff29de42f6d1cf4fab737a791076f3fbb93d7ec
Peter Moser <p.moser@noi.bz.it>
2020-12-23 16:44:19 +0100

    Remove deprecated stuff

--- infrastructure/jenkins/test.groovy
+++ infrastructure/jenkins/test.groovy
@@ -1,33 +1,23 @@
 pipeline {
     agent any
-
     environment {
         COMPOSE_PROJECT_NAME = "e-ink-display-api"
         DOCKER_IMAGE = '755952719952.dkr.ecr.eu-west-1.amazonaws.com/e-ink-displays-api'
         DOCKER_TAG = "test-$BUILD_NUMBER"
-
+		ANSIBLE_LIMIT = "test"
 		SERVER_PORT = "1012"
-
         DB_URL = "jdbc:postgresql://test-pg-bdp.co90ybcr8iim.eu-west-1.rds.amazonaws.com:5432/epaper"
         DB_USERNAME = "epaper"
         DB_PASSWORD = credentials('epaper-api-test-db-password')
-
 		APP_DEBUG = true
-
 		PROXY_ENABLED = false
 		PROXY_URL = "http://localhost:19998"
-
 		WEBSOCKET_ENABLED = true
-
 		NOI_EVENTS_ENABLED = true
 		NOI_CRON_EVENTS = "0 0 0/12 * * ?"
 		NOI_CRON_DISPLAYS = "0 0/10 6-22 * * ?"
-
 		CRON_HEARTBEAT = "0 0 0/1 * * ?"
-
-		API_URL = "https://api.epaper.opendatahub.testingmachine.eu"
     }
-
     stages {
         stage('Configure') {
             steps {
@@ -36,29 +26,18 @@ pipeline {
                     echo 'COMPOSE_PROJECT_NAME=${COMPOSE_PROJECT_NAME}' >> .env
                     echo 'DOCKER_IMAGE=${DOCKER_IMAGE}' >> .env
                     echo 'DOCKER_TAG=${DOCKER_TAG}' >> .env
-
 					echo 'SERVER_PORT=${SERVER_PORT}' >> .env
-
                     echo 'DB_URL=${DB_URL}' >> .env
                     echo 'DB_USERNAME=${DB_USERNAME}' >> .env
                     echo 'DB_PASSWORD=${DB_PASSWORD}' >> .env
-
 					echo 'APP_DEBUG=${APP_DEBUG}' >> .env
-
 					echo 'PROXY_ENABLED=${PROXY_ENABLED}' >> .env
 					echo 'PROXY_URL=${PROXY_URL}' >> .env
-
 					echo 'WEBSOCKET_ENABLED=${WEBSOCKET_ENABLED}' >> .env
-
 					echo 'NOI_EVENTS_ENABLED=${NOI_EVENTS_ENABLED}' >> .env
 					echo 'NOI_CRON_EVENTS=${NOI_CRON_EVENTS}' >> .env
 					echo 'NOI_CRON_DISPLAYS=${NOI_CRON_DISPLAYS}' >> .env
-
 					echo 'CRON_HEARTBEAT=${CRON_HEARTBEAT}' >> .env
-
-					cd proxy
-					rm -f .env
-					echo 'API_URL=${API_URL} >> .env'
                 """
             }
         }
@@ -83,8 +62,9 @@ pipeline {
             steps {
                sshagent(['jenkins-ssh-key']) {
                     sh """
-                        (cd infrastructure/ansible && ansible-galaxy install -f -r requirements.yml)
-                        (cd infrastructure/ansible && ansible-playbook --limit=test deploy.yml --extra-vars "release_name=${BUILD_NUMBER}")
+						cd infrastructure/ansible
+                        ansible-galaxy install -f -r requirements.yml
+                        ansible-playbook --limit=${ANSIBLE_LIMIT} deploy.yml --extra-vars "release_name=${BUILD_NUMBER}"
                     """
                 }
             }
