bcb98664a9e3991a024e3758212ce6c18cad7161
Saimonas Vaičkus <saimonas.vaickus@fos.it>
2022-01-19 22:32:27 +0200

    Remove more unused code related to old image exchange system

--- src/main/java/it/noi/edisplay/components/EventsScheduler.java
+++ src/main/java/it/noi/edisplay/components/EventsScheduler.java
@@ -1,11 +1,6 @@
 package it.noi.edisplay.components;
 
-
 import it.noi.edisplay.dto.EventDto;
-import it.noi.edisplay.model.Connection;
-import it.noi.edisplay.model.Display;
-import it.noi.edisplay.repositories.*;
-import it.noi.edisplay.services.EDisplayRestService;
 import it.noi.edisplay.services.OpenDataRestService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,25 +19,11 @@ public class EventsScheduler {
 
 	private static final Logger logger = LoggerFactory.getLogger(EventsScheduler.class);
 
-
 	@Value("${cron.enabled}")
 	private Boolean enabled;
 
 	@Autowired
 	private OpenDataRestService openDataRestService;
-
-	@Autowired
-	private DisplayRepository displayRepository;
-
-	@Autowired
-	private TemplateRepository templateRepository;
-
-
-	@Autowired
-	private ConnectionRepository connectionRepository;
-
-	@Autowired
-	private EDisplayRestService eDisplayRestService;
 
 	private ArrayList<EventDto> events;
 
@@ -66,67 +47,7 @@ public class EventsScheduler {
 	@Scheduled(cron = "${cron.opendata.displays}")
 	public void updateDisplays() throws IOException {
 		if (enabled) {
-			logger.debug("Send events to display START");
-
-			long currentTime = System.currentTimeMillis();
-
-			//removes events that are finished or will finish in the next 5 minutes
-			events.removeIf(eventDto -> eventDto.getRoomEndDateUTC() - 300000 < currentTime);
-
-			ArrayList<String> checkedDisplays = new ArrayList<>();
-
-			for (EventDto event : events) {
-				String roomName = event.getSpaceDesc().replace("NOI ", "") + " Display";
-
-
-				if (roomName.contains("+")) {
-					String roomNumbers = roomName.replaceAll("\\D+", ""); //removes all non digits
-					// assuming that rooms are Seminar rooms, since no other rooms have this kind of notation
-					for (char roomNumber : roomNumbers.toCharArray()) {
-						logger.debug("EVENT_SCHEDULER: Event with multiple rooms found: Seminar " + roomNumber);
-						String seminarRoomName = "Seminar " + roomNumber + " Display";
-						Display display = displayRepository.findByName(seminarRoomName); //needs to be optimized, if name changes it doesn't work anymore
-
-
-						if (display != null) {
-							if (!checkedDisplays.contains(display.getUuid())) {
-								//check display/event mapping: if display not in list or event different, update event
-								if (!displayUuidToEventMapping.containsKey(display.getUuid()) || !displayUuidToEventMapping.get(display.getUuid()).equals(event.getEventDescriptionEN())) {
-									//update display
-									logger.debug("EVENT_SCHEDULER: Event updated to " + event.getEventDescriptionEN() + " for display " + seminarRoomName);
-									display.setTemplate(templateRepository.findByName(DefaultDataLoader.EVENT_TEMPLATE_NAME));
-									Display savedDisplay = displayRepository.save(display);
-									Connection connection = connectionRepository.findByDisplay(savedDisplay);
-									displayUuidToEventMapping.put(display.getUuid(), event.getEventDescriptionEN());
-									logger.debug("EVENT_SCHEDULER: Send image");
-									eDisplayRestService.sendImageToDisplayAsync(connection, false);
-								}
-								checkedDisplays.add(display.getUuid());
-							}
-						}
-					}
-				} else {
-					Display display = displayRepository.findByName(roomName); //needs to be optimized, if name changes it doesn't work anymore
-					if (display != null) {
-						if (!checkedDisplays.contains(display.getUuid())) {
-
-							if (!displayUuidToEventMapping.containsKey(display.getUuid()) || !displayUuidToEventMapping.get(display.getUuid()).equals(event.getEventDescriptionEN())) {
-								logger.debug("EVENT_SCHEDULER: Event updated to " + event.getEventDescriptionEN() + " for display " + roomName);
-								display.setTemplate(templateRepository.findByName(DefaultDataLoader.EVENT_TEMPLATE_NAME));
-								Display savedDisplay = displayRepository.save(display);
-								Connection connection = connectionRepository.findByDisplay(savedDisplay);
-								displayUuidToEventMapping.put(display.getUuid(), event.getEventDescriptionEN());
-								logger.debug("EVENT_SCHEDULER: Send image");
-								eDisplayRestService.sendImageToDisplayAsync(connection, false);
-							}
-							checkedDisplays.add(display.getUuid());
-						}
-					}
-				}
-
-			}
-
-			logger.debug("Send events to display END");
+		    //TODO: implement content update for displays
 		}
 	}
 
--- src/main/java/it/noi/edisplay/controller/DisplayController.java
+++ src/main/java/it/noi/edisplay/controller/DisplayController.java
@@ -2,10 +2,8 @@ package it.noi.edisplay.controller;
 
 
 import it.noi.edisplay.dto.DisplayDto;
-import it.noi.edisplay.dto.StateDto;
 import it.noi.edisplay.model.*;
 import it.noi.edisplay.repositories.*;
-import it.noi.edisplay.services.EDisplayRestService;
 import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,14 +13,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Random;
-
-import static it.noi.edisplay.components.DefaultDataLoader.EVENT_TEMPLATE_NAME;
-
 
 /**
  * Controller class to create API for CRUD operations on Displays
@@ -47,13 +40,8 @@ public class DisplayController {
 	@Autowired
 	private LocationRepository locationRepository;
 
-
 	@Autowired
 	private ModelMapper modelMapper;
-
-	@Autowired
-	private EDisplayRestService eDisplayRestService;
-
 
 	private Logger logger = LoggerFactory.getLogger(DisplayController.class);
 
@@ -68,74 +56,6 @@ public class DisplayController {
 		logger.debug("Get display with uuid: " + uuid);
 		return new ResponseEntity<>(modelMapper.map(display, DisplayDto.class), HttpStatus.OK);
 	}
-
-
-	@RequestMapping(value = "/send", method = RequestMethod.POST)
-	public ResponseEntity send(@RequestParam("uuid") String uuid, @RequestParam("inverted") Boolean inverted) throws IOException {
-		Display display = displayRepository.findByUuid(uuid);
-		StateDto currentState;
-		if (display != null) {
-			Connection connection = connectionRepository.findByDisplay(display);
-			if (connection != null) {
-				logger.debug("Sending image to display with uuid:" + uuid);
-				currentState = eDisplayRestService.sendImageToDisplay(connection, inverted);
-				display.setLastState(new Date());
-				displayRepository.save(display);
-				currentState.setLastState(display.getLastState());
-				logger.debug("Image successful send to display with uuid " + uuid);
-				return new ResponseEntity(currentState, HttpStatus.OK);
-			} else {
-				logger.debug("Sending image to display with uuid:" + uuid + " failed. Connection not found");
-				currentState = new StateDto("No connection found");
-				currentState.setLastState(display.getLastState());
-			}
-		} else {
-			logger.debug("Sending image to display with uuid:" + uuid + " failed. Display not found");
-			currentState = new StateDto("No display found");
-		}
-		return new ResponseEntity(currentState, HttpStatus.BAD_REQUEST);
-
-	}
-
-	@RequestMapping(value = "/state/{uuid}", method = RequestMethod.GET)
-	public ResponseEntity getState(@PathVariable("uuid") String uuid) throws IOException {
-		Display display = displayRepository.findByUuid(uuid);
-		if (display != null) {
-			Connection connection = connectionRepository.findByDisplay(display);
-			if (connection != null) {
-				display.setLastState(new Date());
-				displayRepository.save(display);
-				StateDto currentState = eDisplayRestService.getCurrentState(connection);
-				currentState.setLastState(display.getLastState());
-				logger.debug("Get state of display with uuid:" + uuid);
-				return new ResponseEntity<>(currentState, HttpStatus.OK);
-			} else
-				logger.debug("Get state of display with uuid:" + uuid + " failed. Connection not found");
-		} else
-			logger.debug("Get state of display with uuid:" + uuid + " failed. Display not found");
-		return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-	}
-
-	@RequestMapping(value = "/clear/{uuid}", method = RequestMethod.POST)
-	public ResponseEntity clear(@PathVariable("uuid") String uuid) {
-		Display display = displayRepository.findByUuid(uuid);
-		if (display != null) {
-			Connection connection = connectionRepository.findByDisplay(display);
-			if (connection != null) {
-				logger.debug("Clear display with uuid:" + uuid);
-				display.setLastState(new Date());
-				display.setErrorMessage("");
-				displayRepository.save(display);
-				StateDto currentState = eDisplayRestService.clearDisplay(connection);
-				currentState.setLastState(display.getLastState());
-				return new ResponseEntity(currentState, HttpStatus.OK);
-			} else
-				logger.debug("Failed to clear display with uuid:" + uuid + ". Connection not found");
-		} else
-			logger.debug("Failed to clear display with uuid:" + uuid + ". Display not found");
-		return new ResponseEntity(HttpStatus.BAD_REQUEST);
-	}
-
 
 	@RequestMapping(value = "/all", method = RequestMethod.GET)
 	public ResponseEntity getAllDisplays() {
@@ -184,99 +104,6 @@ public class DisplayController {
 		logger.debug("Display with uuid:" + savedDisplay.getUuid() + " created.");
 		return new ResponseEntity<>(modelMapper.map(savedDisplay, DisplayDto.class), HttpStatus.CREATED);
 	}
-
-	@RequestMapping(value = "/auto-create", method = RequestMethod.POST)
-	public ResponseEntity autoCreateDisplay(@RequestParam("name") String name, @RequestParam("ip") String ip, @RequestParam("width") int width, @RequestParam("height") int height, @RequestParam("mac") String mac) throws IOException {
-
-		Connection connectionByMac = connectionRepository.findByMac(mac);
-
-		if (connectionByMac == null) {
-			//check if name already exists and connect with that display
-
-			Display displayByName = displayRepository.findByName(name);
-
-			if (displayByName != null) {
-				logger.debug("AUTO-CREATE: RECONNECT TO DISPLAY WITH NAME " + name);
-
-				Connection connectionByDisplay = connectionRepository.findByDisplay(displayByName);
-				connectionByDisplay.setMac(mac);
-				connectionByDisplay.setNetworkAddress(ip);
-
-				connectionRepository.save(connectionByDisplay);
-
-				logger.debug("AUTO-CREATE: FINISHED WITH NEW IP " + ip);
-
-				StateDto state = eDisplayRestService.sendImageToDisplay(connectionByDisplay, false);
-				if (state.getErrorMessage() != null) {
-					connectionByDisplay.setConnected(false);
-					logger.debug("Trying to connect to physical display failed with error: " + state.getErrorMessage());
-				} else {
-					connectionByDisplay.setConnected(true);
-					logger.debug("AUTO-CREATE: Image sent");
-				}
-
-				connectionRepository.save(connectionByDisplay);
-				logger.debug("AUTO-CREATE: COMPLETED");
-			} else {
-
-				logger.debug("AUTO-CREATE: CREATE STARTED");
-				Display display = new Display();
-				display.setName(name);
-				display.setBatteryPercentage(new Random().nextInt(99));
-
-				display.setTemplate(templateRepository.findByName(EVENT_TEMPLATE_NAME));
-
-				Resolution resolutionbyWidthAndHeight = resolutionRepository.findByWidthAndHeight(width, height);
-				if (resolutionbyWidthAndHeight == null) {
-					Resolution resolution = new Resolution(width, height);
-					resolutionRepository.saveAndFlush(resolution);
-					display.setResolution(resolution);
-				} else
-					display.setResolution(resolutionbyWidthAndHeight);
-
-				Display savedDisplay = displayRepository.saveAndFlush(display);
-
-				logger.debug("AUTO-CREATE: Display with uuid:" + savedDisplay.getUuid() + " created.");
-
-				Connection connection = new Connection();
-				Location location = locationRepository.findByName("Meeting Room");
-
-				connection.setDisplay(savedDisplay);
-				connection.setNetworkAddress(ip);
-				connection.setLocation(location);
-				connection.setMac(mac);
-				connection.setCoordinates(new Point(0, 0));
-
-				StateDto state = eDisplayRestService.sendImageToDisplay(connection, false);
-				if (state.getErrorMessage() != null) {
-					connection.setConnected(false);
-					logger.debug("Trying to connect to physical display failed with error: " + state.getErrorMessage());
-				} else {
-					connection.setConnected(true);
-					logger.debug("AUTO-CREATE: Image sent to:" + savedDisplay.getUuid());
-				}
-
-				Connection savedConnection = connectionRepository.save(connection);
-				logger.debug("AUTO-CREATE: Connection with uuid:" + savedConnection.getUuid() + " created.");
-			}
-		} else {
-			logger.debug("AUTO-CREATE: RECONNECT BY MAC ADDRESS STARTED");
-			connectionByMac.setNetworkAddress(ip);
-			StateDto state = eDisplayRestService.sendImageToDisplay(connectionByMac, false);
-			if (state.getErrorMessage() != null) {
-				connectionByMac.setConnected(false);
-				logger.debug("Trying to connect to physical display failed with error: " + state.getErrorMessage());
-			} else {
-				connectionByMac.setConnected(true);
-				connectionByMac.setNetworkAddress(ip);
-				logger.debug("AUTO-CREATE: Connection with uuid:" + connectionByMac.getUuid() + " has new IP " + ip);
-			}
-			connectionRepository.save(connectionByMac);
-		}
-
-		return new ResponseEntity<>(HttpStatus.CREATED);
-	}
-
 
 	@RequestMapping(value = "/simple-create", method = RequestMethod.POST)
 	public ResponseEntity simpleCreateDisplay(@RequestParam("name") String name, @RequestParam("templateUuid") String templateUuid, @RequestParam("width") int width, @RequestParam("height") int height, @RequestParam("networkAddress") String networkAddress, @RequestParam("locationUuid") String locationUuid) {
@@ -367,22 +194,4 @@ public class DisplayController {
 		logger.debug("Updated display with uuid:" + display.getUuid());
 		return new ResponseEntity(modelMapper.map(displayRepository.saveAndFlush(display), DisplayDto.class), HttpStatus.ACCEPTED);
 	}
-
-	@RequestMapping(value = "/testproxyconnection", method = RequestMethod.GET)
-	public ResponseEntity<String> test() {
-		logger.debug("Connection with with Proxy established.");
-		String response = eDisplayRestService.testProxy();
-		return new ResponseEntity<>(response, HttpStatus.OK);
-	}
-
-	@RequestMapping(value = "/proxy-register", method = RequestMethod.POST)
-	public ResponseEntity<String> proxyRegister(@RequestParam("url") String url) {
-		logger.debug("Registering new proxy URL: " + url);
-		eDisplayRestService.setProxyIpAddress(url);
-		String response = eDisplayRestService.testProxy();
-		logger.debug("Registering done");
-		return new ResponseEntity<>(response, HttpStatus.OK);
-	}
-
-
 }
