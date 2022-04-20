7015280e1b90bb2b2648b69b594c0dbdf8345f39
Saimonas Vaičkus <saimonas.vaickus@fos.it>
2022-04-20 12:39:16 +0300

    Removed all code and files related to proxies

--- infrastructure/utils/run-proxy-dockerfile.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker build -t epaper-proxy -f infrastructure/docker/proxy.dockerfile .
-docker run -it epaper-proxy sh
-
-echo "API_URL=https://api.epaper.opendatahub.testingmachine.eu" > .env
-touch local-tunnel.log
