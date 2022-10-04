12834b34537a803d429903206d4fe5ad9473997a
Simon Dalvai <s.dalvai@noi.bz.it>
2022-10-04 11:27:12 +0200

    use production tourism api for events url

--- src/main/java/it/noi/edisplay/services/OpenDataRestService.java
+++ src/main/java/it/noi/edisplay/services/OpenDataRestService.java
@@ -17,7 +17,7 @@ import java.util.List;
 public class OpenDataRestService {
 
 	private final RestTemplate restTemplate;
-	private String eventsUrl = "https://api.tourism.testingmachine.eu/v1/EventShort/GetbyRoomBooked?startdate=%s&eventlocation=NOI&datetimeformat=uxtimestamp&onlyactive=true";
+	private String eventsUrl = "https://tourism.opendatahub.bz.it/v1/EventShort/GetbyRoomBooked?startdate=%s&eventlocation=NOI&datetimeformat=uxtimestamp&onlyactive=true";
 	private String eventLocationUrl = "http://tourism.opendatahub.bz.it/api/EventShort/RoomMapping";
 	private String placesUrl = "https://mobility.api.opendatahub.bz.it/v2/flat/NOI-Place?select=scode,smetadata.name.en,smetadata.room_label,smetadata.todaynoibzit&limit=-1&where=and(smetadata.type.in.(Meetingroom,Seminarroom),sorigin.neq.office365,sactive.eq.true)";
 
