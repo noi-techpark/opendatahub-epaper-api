f191a382a334cf1c334b8d51aa30d34364490760
Simon Dalvai <s.dalvai@noi.bz.it>
2022-12-15 11:33:05 +0100

    fix tourism api endpoint

--- src/main/java/it/noi/edisplay/services/OpenDataRestService.java
+++ src/main/java/it/noi/edisplay/services/OpenDataRestService.java
@@ -17,8 +17,8 @@ import java.util.List;
 public class OpenDataRestService {
 
 	private final RestTemplate restTemplate;
-	private String eventsUrl = "https://tourism.opendatahub.com/v1/EventShort/GetbyRoomBooked?startdate=%s&eventlocation=NOI&datetimeformat=uxtimestamp&onlyactive=true";
-	private String eventLocationUrl = "http://tourism.opendatahub.com/api/EventShort/RoomMapping";
+	private String eventsUrl = "https://tourism.api.opendatahub.com/v1/EventShort/GetbyRoomBooked?startdate=%s&eventlocation=NOI&datetimeformat=uxtimestamp&onlyactive=true";
+	private String eventLocationUrl = "https://tourism.api.opendatahub.com/v1/EventShort/RoomMapping";
 	private String placesUrl = "https://mobility.api.opendatahub.com/v2/flat/NOI-Place?select=scode,smetadata.name.en,smetadata.room_label,smetadata.todaynoibzit&limit=-1&where=and(smetadata.type.in.(Meetingroom,Seminarroom),sorigin.neq.office365,sactive.eq.true)";
 
 
