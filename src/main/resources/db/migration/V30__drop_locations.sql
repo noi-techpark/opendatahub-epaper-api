74a47a7a8f3eb968a4940c19a7b32cba7b4e1ad2
Simon Dalvai <s.dalvai@noi.bz.it>
2023-10-24 09:42:05 +0200

    fix drop_locations sql

--- src/main/resources/db/migration/V30__drop_locations.sql
+++ src/main/resources/db/migration/V30__drop_locations.sql
@@ -1,15 +1,13 @@
-ALTER TABLE IF EXISTS public.scheduled_content
-    ALTER room_codes DROP default,
-    ALTER room_codes TYPE text[] USING array[room_codes],
-    ALTER room_codes SET default '{}';
+ALTER TABLE IF EXISTS public.displays
+    ADD COLUMN room_codes text[];
 
-UPDATE public.display d
-    set d.room_codes = l.room_codes
-    from public.locations l
-    where l.id = d.location_id;
+UPDATE public.displays
+    SET room_codes = l.room_codes
+    FROM public.locations l
+    WHERE l.id = location_id;
 
 ALTER TABLE  public.displays
 DROP COLUMN IF EXISTS location_id;
 
-DROP TABLE IF EXISTS public.display_locations;
+DROP TABLE IF EXISTS public.displays_locations;
 DROP TABLE IF EXISTS public.locations;
