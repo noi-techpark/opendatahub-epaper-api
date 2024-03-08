27c3f71997c2705bd14cb9246752178318f96417
ornedalecini <161297442+ornedaleciniFos@users.noreply.github.com>
2024-03-08 15:04:07 +0100

    Add files via upload

--- src/main/java/it/noi/edisplay/dto/TemplateDto.java
+++ src/main/java/it/noi/edisplay/dto/TemplateDto.java
@@ -4,16 +4,25 @@
 
 package it.noi.edisplay.dto;
 
-import io.swagger.annotations.ApiModelProperty;
-
 import java.sql.Date;
+
+import io.swagger.annotations.ApiModelProperty;
 
 public class TemplateDto {
 
     private String name;
 
     private String description;
-    
+
+    private boolean footer;
+
+    private boolean header;
+
+    private boolean multipleRoom;
+
+    private ResolutionDto resolution;
+
+    private int[] roomData;
     private DisplayContentDto displayContent;
 
     @ApiModelProperty(hidden = true)
@@ -69,5 +78,45 @@ public class TemplateDto {
 
     public void setDisplayContent(DisplayContentDto displayContent) {
         this.displayContent = displayContent;
+    }
+
+    public boolean getFooter() {
+        return footer;
+    }
+
+    public void setFooter(boolean footer) {
+        this.footer = footer;
+    }
+
+    public boolean getHeader() {
+        return header;
+    }
+
+    public void setHeader(boolean header) {
+        this.header = header;
+    }
+
+    public boolean getMultipleRoom() {
+        return multipleRoom;
+    }
+
+    public void setMultipleRoom(boolean multipleRoom) {
+        this.multipleRoom = multipleRoom;
+    }
+
+    public int[] getRoomData() {
+        return roomData;
+    }
+
+    public void setRoomData(int[] roomData) {
+        this.roomData = roomData;
+    }
+
+    public ResolutionDto getResolution() {
+        return resolution;
+    }
+
+    public void setResolution(ResolutionDto resolution) {
+        this.resolution = resolution;
     }
 }
