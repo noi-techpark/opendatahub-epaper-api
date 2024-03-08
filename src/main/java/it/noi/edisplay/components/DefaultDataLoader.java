2b27e06b0727a0a700ea7469bf3c20f68a988eae
ornedalecini <161297442+ornedaleciniFos@users.noreply.github.com>
2024-03-08 15:09:03 +0100

    Add files via upload

--- src/main/java/it/noi/edisplay/components/DefaultDataLoader.java
+++ src/main/java/it/noi/edisplay/components/DefaultDataLoader.java
@@ -4,65 +4,103 @@
 
 package it.noi.edisplay.components;
 
-import it.noi.edisplay.model.*;
-import it.noi.edisplay.repositories.*;
+import java.io.IOException;
+
+import javax.annotation.PostConstruct;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
-import java.io.IOException;
+import it.noi.edisplay.model.Resolution;
+import it.noi.edisplay.model.Template;
+import it.noi.edisplay.repositories.ResolutionRepository;
+import it.noi.edisplay.repositories.TemplateRepository;
 
 @Component
 public class DefaultDataLoader {
 
-	public static final String EVENT_TEMPLATE_NAME = "NOI Template";
-	private Logger logger = LoggerFactory.getLogger(DefaultDataLoader.class);
-	@Value("${cron.enabled}")
-	private Boolean enabled;
+    public static final String EVENT_TEMPLATE_NAME = "NOI Template";
+    private Logger logger = LoggerFactory.getLogger(DefaultDataLoader.class);
+    @Value("${cron.enabled}")
+    private Boolean enabled;
 
-	@Autowired
-	private TemplateRepository templateRepository;
+    @Autowired
+    private TemplateRepository templateRepository;
 
-	@Autowired
-	private ResolutionRepository resolutionRepository;
+    @Autowired
+    private ResolutionRepository resolutionRepository;
 
-	@PostConstruct
-	public void onStartUp() throws IOException {
+    @PostConstruct
+    public void onStartUp() throws IOException {
 
-		// add default templates
-		addTemplate("Office");
-		addTemplate("Meeting Room");
-		addTemplate("Free Software Lab");
-		addTemplate(EVENT_TEMPLATE_NAME);
+        if (templateRepository.findAll().size() == 0) {
 
-		// add default resolutions
-		addResolution(1440, 2560, 24);
-		addResolution(1872, 1404, 4);
-		addResolution(1440, 5120, 24);
-	}
+            Template officeTemplate = new Template();
+            Template meetingRoomTemplate = new Template();
+            Template freeSoftwareLabTemplate = new Template();
+            Template noiTemplate = new Template();
+            Resolution resolution = new Resolution();
+            resolution.setWidth(1440);
+            resolution.setHeight(2560);
+            resolution.setBitDepth(24);
+            Resolution resolutionSmall = new Resolution();
+            resolutionSmall.setWidth(1872);
+            resolutionSmall.setHeight(1404);
+            resolutionSmall.setBitDepth(24);
+            resolutionRepository.save(resolutionSmall);
+            Resolution resolutionMS = new Resolution();
+            resolutionSmall.setWidth(1440);
+            resolutionSmall.setHeight(5120);
+            resolutionSmall.setBitDepth(24);
+            resolutionRepository.save(resolutionMS);
+            resolutionRepository.save(resolution);
+            officeTemplate.setName("Office");
+            meetingRoomTemplate.setName("Meeting Room");
+            freeSoftwareLabTemplate.setName("Free Software Lab");
+            noiTemplate.setName(EVENT_TEMPLATE_NAME);
+            officeTemplate.setResolution(resolution);
+            meetingRoomTemplate.setResolution(resolution);
+            freeSoftwareLabTemplate.setResolution(resolution);
+            noiTemplate.setResolution(resolution);
+            officeTemplate.setFooter(false);
+            meetingRoomTemplate.setFooter(false);
+            freeSoftwareLabTemplate.setFooter(false);
+            noiTemplate.setFooter(false);
+            officeTemplate.setHeader(false);
+            meetingRoomTemplate.setHeader(false);
+            freeSoftwareLabTemplate.setHeader(false);
+            noiTemplate.setHeader(false);
+            officeTemplate.setMultipleRoom(false);
+            meetingRoomTemplate.setMultipleRoom(false);
+            freeSoftwareLabTemplate.setMultipleRoom(false);
+            noiTemplate.setMultipleRoom(false);
 
-	private void addResolution(int width, int height, int bitDepth) {
-		if (resolutionRepository.findByWidthAndHeightAndBitDepth(width, height, bitDepth) == null) {
-			Resolution resolution = new Resolution();
-			resolution.setWidth(width);
-			resolution.setHeight(height);
-			resolution.setBitDepth(bitDepth);
-			resolutionRepository.saveAndFlush(resolution);
+            templateRepository.save(officeTemplate);
+            templateRepository.save(meetingRoomTemplate);
+            templateRepository.save(noiTemplate);
+            templateRepository.save(freeSoftwareLabTemplate);
 
-			logger.info("New resolution with width: {} height: {} bidDepth: {} added.", width, height, bitDepth);
-		}
-	}
+        }
 
-	private void addTemplate(String name) {
-		if (templateRepository.findByName(name) == null) {
-			Template template = new Template();
-			template.setName(name);
-			templateRepository.saveAndFlush(template);
-
-			logger.info("New template with name: {} added.", name);
-		}
-	}
+        if (resolutionRepository.findAll().size() == 0) {
+            Resolution resolution = new Resolution();
+            resolution.setWidth(1440);
+            resolution.setHeight(2560);
+            resolution.setBitDepth(24);
+            resolutionRepository.save(resolution);
+            Resolution resolutionSmall = new Resolution();
+            resolutionSmall.setWidth(1872);
+            resolutionSmall.setHeight(1404);
+            resolutionSmall.setBitDepth(4);
+            resolutionRepository.save(resolutionSmall);
+            Resolution resolutionMS = new Resolution();
+            resolutionSmall.setWidth(1440);
+            resolutionSmall.setHeight(5120);
+            resolutionSmall.setBitDepth(24);
+            resolutionRepository.save(resolutionMS);
+        }
+    }
 }
