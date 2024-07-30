d91d765c7dd4898270c28fa81b3eacf7207f0f4f
Simon Dalvai <s.dalvai@noi.bz.it>
2024-07-30 10:54:37 +0200

    flyway: add updates for displays to set values if null | 2

--- src/main/resources/db/migration/V37__displays_room_codes_update.sql
+++ src/main/resources/db/migration/V37__displays_room_codes_update.sql
@@ -1,3 +1,3 @@
 UPDATE public.displays
     SET room_codes = array[]::text[]
-    WHERE room_codes IS NULL or (SELECT unnest(room_codes) IS NULL);
+    WHERE room_codes IS NULL OR array_position(room_codes, NULL) IS NOT NULL;
