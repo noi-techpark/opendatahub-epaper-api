d74d166ae18c6c85316841497af5115ae4b0ffe4
Sam Nart <originalsamnart@gmail.com>
2025-06-13 10:38:03 +0000

    fix cron scheduler and import for scheduled annotation

--- src/main/java/it/noi/edisplay/components/NOIDataLoader.java
+++ src/main/java/it/noi/edisplay/components/NOIDataLoader.java
@@ -16,6 +16,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.scheduling.annotation.Scheduled;
 
 import javax.annotation.PostConstruct;
 
